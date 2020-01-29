5263382249585cb65fd85ec902159fc74f82203b
Kamrul Islam <kamrul74k@gmail.com>
2020-01-29 11:14:34 +0600

    Updates

---
+++ precache-manifest.d46d29cc9cf60f19edd72be0643cf9f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7181da3dc3b03e065d235e4206a9f08",
+    "url": "/react-redux-mini-project/index.html"
+  },
+  {
+    "revision": "ec6138a89bc5da847c0e",
+    "url": "/react-redux-mini-project/static/js/2.6666594c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-mini-project/static/js/2.6666594c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1dd6ba22def00de52c8",
+    "url": "/react-redux-mini-project/static/js/main.bd8bec75.chunk.js"
+  },
+  {
+    "revision": "2e69549815ab1dc20f21",
+    "url": "/react-redux-mini-project/static/js/runtime-main.e1153202.js"
+  }
+]);
